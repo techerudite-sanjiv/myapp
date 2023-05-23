d71bca5298106dae0e9324d110bdda0c15a93fa1
Sanjeev Kumar <sanjiv@ttecherudite.com>
2023-05-23 15:45:15 +0530

    add code

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./form.css";
+const Form = () => {
+
+    const [data, setData] = useState(
+        {
+            name: "",
+            address: "",
+            email: "",
+            age: ""
+        }
+    )
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        localStorage.setItem('user_data', JSON.stringify(data))
+    }
+
+    const handleChange = (e) => {
+        let bio_data_name = e.target.name
+        let bio_data_value = e.target.value
+        setData({ ...data, [bio_data_name]: bio_data_value })
+    }
+
+
+    let get_bioData = localStorage.getItem('user_data');
+    console.log(get_bioData, "i am get bio dtata")
+
+    return (
+        <div className="form-box">
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+                <input type="text" name="address" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+                <input type="text" name="email" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+                <input type="text" name="age" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
