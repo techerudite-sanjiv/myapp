fed7851afc93c4da2f69a33af5a16bdff802d970
Sanjeev Kumar <sanjiv@ttecherudite.com>
2023-05-23 16:29:38 +0530

    code added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import Cookies from "js-cookie";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import "./form.css";
+
 const Form = () => {
 
     const [data, setData] = useState(
@@ -15,7 +16,7 @@ const Form = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         localStorage.setItem('user_data', JSON.stringify(data))
-        Cookies.set("bio_data",JSON.stringify(data))
+        Cookies.set("bio_data", JSON.stringify(data))
     }
 
     const handleChange = (e) => {
@@ -25,22 +26,28 @@ const Form = () => {
     }
 
 
-    let get_bioData = localStorage.getItem('user_data');
-    let get_bioData1 = Cookies.get('bio_data');
-    console.log(get_bioData, "i am get bio dtata")
-    console.log(get_bioData1, "i am cookies data")
+    let local_bioData = localStorage.getItem('user_data');
+    let cookie_bioData = Cookies.get('bio_data');
+
+    console.log(local_bioData, "i am local storage data")
+    console.log(cookie_bioData, "i am cookies data")
 
     return (
         <div className="form-box">
             <form onSubmit={handleSubmit}>
+                <label>Name</label> <br />
                 <input type="text" name="name" onChange={(e) => handleChange(e)} />
                 <br /> <br />
-                <input type="text" name="address" onChange={(e) => handleChange(e)} />
-                <br /> <br />
-                <input type="text" name="email" onChange={(e) => handleChange(e)} />
-                <br /> <br />
+                <label>Age</label> <br />
                 <input type="text" name="age" onChange={(e) => handleChange(e)} />
                 <br /> <br />
+                <label>Email</label> <br />
+                <input type="text" name="email" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+                <label>Address</label> <br />
+                <input type="text" name="address" onChange={(e) => handleChange(e)} />
+                <br /> <br />
+
                 <button>Submit</button>
             </form>
         </div>
